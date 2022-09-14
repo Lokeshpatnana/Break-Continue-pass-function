a2fddfe3d13ed3c9b5abe793aab87901c19b1262
PATNANA LOKESWARA RAO <109509575+Lokeshpatnana@users.noreply.github.com>
2022-09-14 20:12:43 +0530

    Add files via upload

---
+++ break_loop.py
@@ -0,0 +1,21 @@
+# For Even Numbers printing
+n=0
+while(n<20):
+    n+=2
+    if n>8:
+        break
+    print(n)
+# -------------------------
+n=0
+while(n<10):
+    n+=1
+    if n==4:
+        break
+    print(n)
+# --------------------------
+n=0
+while(n<20):
+    n+=2
+    if(n==10):
+        break
+    print(n)
